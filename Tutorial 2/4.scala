b415485ea191b416ee02297316b187b00920ebf7
Nisura Indisa <nisuraindisa21@gmail.com>
2022-07-10 21:11:26 +0530

    4 and 4b

--- Tutorial 2/4.scala
+++ Tutorial 2/4.scala
@@ -1,11 +1,9 @@
-def main(args : Array[String]): Unit = {
-    println(salary(40,12))
-}
+def wage(hours:Int):Int=hours*150
 
+def ot(hours:Int):Int=hours*75
 
-def salary(a:Int, b:Int) : Int = {
-    var x = a * 250
-    var y = b * 85
-    var tot = (x + y) * 1.12
-    return tot;
-}
+def income(h1:Int,h2:Int):Int=wage(h1)+ot(h2)
+
+def tax(income:Int):Int=income*.1
+
+def takeHome(h1:Int,h2:Int):Double= income(h1,h2)-tax(income(h1,h2))
---
+++ Tutorial 2/4b.scala
@@ -0,0 +1,7 @@
+def attendees(price : Int) : Int = 120 + ( 15  - price) / 5 * 20
+
+def revenue(price : Int) : Int = attendees(price) * price
+
+def cost(price : Int): Int = 500 + 3 * attendees(price)
+
+def profit(price : Int) : Int = revenue(price) - cost(price)
