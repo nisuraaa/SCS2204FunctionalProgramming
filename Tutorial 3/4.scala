d43b2727380792712c4636ae306238198a4b5648
Nisura Indisa <nisuraindisa21@gmail.com>
2022-07-20 00:16:56 +0530

    5

--- Tutorial 3/4.scala
+++ Tutorial 3/4.scala
@@ -1,8 +1,8 @@
 object q4{
     def main(args: Array[String]) = {
         def matchTest(x: Int): Double = x>50 match
-        case true => 24.95 * 0.6 + 50 * 3 + (x-50) * 0.75
-        case false => 24.95 * 0.6 + x * 3 
+        case true => 24.95 * 0.6 * x + 50 * 3 + (x-50) * 0.75
+        case false => 24.95 * 0.6 * x + x * 3 
        
         println(matchTest(60))
     }
---
+++ Tutorial 3/5.scala
@@ -0,0 +1,9 @@
+object q5{
+    def main(args: Array[String]) = {
+        def easyP(easypace :Int): Int = return easypace * 8
+
+        def temoP(tempo :Int): Int = return tempo * 7
+        
+        println(easyP(4) + temoP(3))
+    }
+}
