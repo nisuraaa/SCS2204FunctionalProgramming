b1f796496d0f72ad836615f21c061ee05aa286c1
Nisura Indisa <nisuraindisa21@gmail.com>
2022-07-19 23:20:23 +0530

    4.scala

--- Tutorial 3/4.scala
+++ Tutorial 3/4.scala
@@ -1,6 +1,9 @@
 object q4{
     def main(args: Array[String]) = {
-        def cost(c : Int): Double = 24.95 * 0.6 + 3 *  
-        println(convert(35))
+        def matchTest(x: Int): Double = x>50 match
+        case true => 24.95 * 0.6 + 50 * 3 + (x-50) * 0.75
+        case false => 24.95 * 0.6 + x * 3 
+       
+        println(matchTest(60))
     }
 }
