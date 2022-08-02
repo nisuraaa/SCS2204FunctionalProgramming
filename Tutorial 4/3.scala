bb567c9778ef57ec5b9ce67b33ed70982eb1f6cb
Nisura Indisa <nisuraindisa21@gmail.com>
2022-08-03 00:56:42 +0530

    4

--- Tutorial 4/3.scala
+++ Tutorial 4/3.scala
@@ -1,12 +1,32 @@
-import scala.io.StdIn.readLine   
+import scala.io.StdIn.readLine
 
+object q3 {
 
-object q3{
-    def main(args:Array[String])={
-        val a = readLine()
-        println(toUpper(a))
+  def toUpper(text: String) = text.toUpperCase()
+  def toLower(text: String) = text.toLowerCase()
+  def formatNames(
+      name: String
+  )(tdList: Int*)(func: String => String): String = {
+    if (tdList.isEmpty) {
+      return func(name)
     }
+    var str = ""
+    var i = 0
+    while (i < name.length()) {
+      if (tdList.contains(i)) {
+        str = str + func(name(i).toString())
+      } else {
+        str = str + name(i).toString()
+      }
+      i = i + 1
+    }
+    return str
+  }
+  def main(args: Array[String]) = {
 
-    def toUpper(text :String) = text.toUpperCase()
-    def toLower(text :String) = text.toLowerCase()
+    println(formatNames("Benny")()(toUpper))
+    println(formatNames("Niroshan")(1)(toUpper))
+    println(formatNames("Saman")()(toLower))
+    println(formatNames("Kumara")(5)(toUpper))
+  }
 }
